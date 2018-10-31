dc918304ab5916d265bf6414b2f5d67c090d2b75
Maxim Yefimovich <xim97@mail.ru>
2018-10-31 12:17:55 +0300

    some fixes

--- src/extensions/promts/PromtsApplicationCustomizer.ts
+++ src/extensions/promts/PromtsApplicationCustomizer.ts
@@ -16,50 +16,46 @@ export default class PromtsApplicationCustomizer
 
     @override
     public onInit(): Promise<void> {
+        debugger;
         const data: Array<any> = [
             {
                 title: "Options",
                 message: "Call the \"Options \" menu to access personal and application settings",
                 link: "link1",
-                selector: "#O365_MainLink_Settings"
+                selector: "#O365_MainLink_Settings",
+                isShowed: false
             },
             {
                 title: "Search",
                 message: "Search this site",
                 link: "link2",
-                selector: "#spPageChromeAppDiv > div > div > div:nth-child(2) > div > div.ms-compositeHeader-siteAndActionsContainer > div.ms-compositeHeader-actionsContainer > div.ms-compositeHeader-searchBoxContainer > div.searchBoxContainer_b51fc60b > div > div > form > input"
+                selector: "#spPageChromeAppDiv > div > div > div:nth-child(2) > div > div.ms-compositeHeader-siteAndActionsContainer > div.ms-compositeHeader-actionsContainer > div.ms-compositeHeader-searchBoxContainer > div.searchBoxContainer_b51fc60b > div > div > form > input",
+                isShowed: false
             },
             {
                 title: "Application Launcher",
                 message: "Opening the launcher to access Office 365 applications",
                 link: "link3",
-                selector: "#O365_MainLink_NavMenu"
+                selector: "#O3165_MainLink_NavMenu",
+                isShowed: false
             }
         ];
         const instanceId: string = this.componentId;
-        const user: string = this.context.pageContext.user.loginName;        
+        const user: string = this.context.pageContext.user.loginName;
         this.tryToRenderPromts(data, instanceId, user);
         return Promise.resolve();
     }
 
-    private handleClickOnDocument(event): void {
+    private handleClickOnDocument(event: any, promtsContainers: Array<any>): void {
         if (!this.isPromtElement(event.target)) {
-            this.removePromts();
+            this.removePromts(promtsContainers);
         }
     }
 
-    private removePromts(): void {
-        let promts: NodeListOf<Element> = document.body.querySelectorAll(".ms-Layer.ms-Layer--fixed");
-        for (let i: number = 0; i < promts.length; i++) {
-            this.removeChild(promts[i]);
-        }
-    }
-
-    private removeChild(rootNode: Element): void {
-        let element: any = rootNode;
-        while (element.lastChild) {
-            element.removeChild(element.lastChild);
-        }
+    private removePromts(containers: Array<any>): void {
+        containers.forEach(container => {
+            ReactDOM.unmountComponentAtNode(container);
+        });
     }
 
     private isPromtElement(element: any): boolean {
@@ -70,7 +66,7 @@ export default class PromtsApplicationCustomizer
         return currentElement !== document.body;
     }
 
-    private renderPromt(item: any, instanceId: string, user: string): void {
+    private renderPromt(item: any, instanceId: string, user: string): any {
         let element: any = document.body.querySelector(item.selector);
         const reactElement: React.ReactElement<ICustomCalloutProps> = React.createElement(
             CustomCallout,
@@ -85,22 +81,47 @@ export default class PromtsApplicationCustomizer
         );
         element.appendChild(document.createElement("div"));
         ReactDOM.render(reactElement, element.lastChild);
+        return element.lastChild;
     }
 
     private tryToRenderPromts(data: Array<any>, instanceId: string, user: string): void {
         const that: any = this;
+        let promtsContainers: Array<any> = [];
+        let tryToRenderPromtResult: [boolean, any];
+        let numberOfShowedPromts: number = 0;
+        let numberOfCycles: number = 0;
         const intervalID: number = setInterval(() => {
-            if (data !== undefined && that.checkSelectors(data)) {
+            if (data !== undefined) {
                 data.forEach(item => {
-                    that.renderPromt(item, instanceId, user);
+                    if (!item.isShowed) {
+                        tryToRenderPromtResult = that.tryToRenderPromt(item, instanceId, user);
+                        item.isShowed = tryToRenderPromtResult[0];
+                        if (tryToRenderPromtResult[1] !== undefined) {
+                            promtsContainers.push(tryToRenderPromtResult[1]);
+                            numberOfShowedPromts++;
+                        }
+                    }
                 }
                 );
-                document.body.addEventListener("click", event => {
-                    that.handleClickOnDocument(event);
-                });
+                if (numberOfShowedPromts === data.length || numberOfCycles > 200) {
+                    clearInterval(intervalID);                   
+                }               
+            } else {
                 clearInterval(intervalID);
             }
+            numberOfCycles++;
         }, 250);
+        document.body.addEventListener("click", event => {
+            that.handleClickOnDocument(event, promtsContainers);
+        });
+    }
+
+    private tryToRenderPromt(item: any, instanceId: string, user: string): [boolean, any] {
+        if (document.body.querySelector(item.selector) !== null) {
+            return [true, this.renderPromt(item, instanceId, user)];
+        } else {
+            return [false, undefined];
+        }
     }
 
     private checkSelectors(data: Array<any>): boolean {
